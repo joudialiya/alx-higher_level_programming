562e260345138f90c7f937f6fd80a6de08658844
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-05 10:49:07 -0700

    [task 13, handling the cas where number is smaller than head's n]

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -17,21 +17,19 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 	new->n = number;
 	current = *head;
+	if (*head == NULL || number < (*head)->n)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	/*
 	 * searcing for the node that is just before
 	 * the node that is grater than number
 	 */
-	while (current && current->next && number > current->next->n)
+	while (current->next && number > current->next->n)
 		current = current->next;
-	if (current == NULL)
-	{
-		new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		new->next = current->next;
-		current->next = new;
-	}
+	new->next = current->next;
+	current->next = new;
 	return (new);
 }
