a39b03ac18b39b4714d9454da48e6c6c24e50d51
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-05 09:22:48 -0700

    [task 1, handling negative numbers]

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+if number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
 print(f"Last digit of {number:d} is {last_digit:d}", end=" ")
 if last_digit == 0:
     print("and is 0")
