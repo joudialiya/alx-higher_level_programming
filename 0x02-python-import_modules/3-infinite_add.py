f899390035c1bd49d55cb1441a056c3b937e8bd0
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-07 04:47:53 -0700

    [3. the script sould not be executable when imported]

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-sum = 0
-for i in range(1, len(argv)):
-    sum += int(argv[i])
-print("{:d}".format(sum))
+
+
+def main():
+    sum = 0
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print("{:d}".format(sum))
+
+
+if __name__ == "__main__":
+    main()
