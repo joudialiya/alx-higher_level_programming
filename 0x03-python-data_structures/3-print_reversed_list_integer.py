aa57ff9f6e44918c79e072cafcfe19126bfab0f6
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-10 02:48:12 -0700

    task 3 so basically list.reverse() no not return the list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for i in my_list.copy().reverse():
+    cpy = my_list.copy()
+    cpy.reverse()
+    for i in cpy:
         print("{:d}".format(i))
