bbca107c1daeadd0e51275cf19d7e9dfc19e7a93
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-11 04:47:13 -0700

    task 8, we are supposed to return the length as the first component

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,4 +4,4 @@ def multiple_returns(sentense):
     first_char = None
     if len(sentense) > 0:
         first_char = sentense[0]
-    return (first_char, len(sentense))
+    return (len(sentense), first_char)
