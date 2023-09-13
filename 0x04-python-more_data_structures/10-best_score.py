67e72ad7b6f6a9e2c37efc0d4c14243b40b5df2b
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-13 12:07:52 -0700

    task 10, should check for None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     m = (None, None)
-    for key, value in a_dictionary.items():
-        if (not m[1]) or value > m[1]:
-            m = (key, value)
+    if dictionary:
+        for key, value in a_dictionary.items():
+            if (not m[1]) or value > m[1]:
+                m = (key, value)
     return (m[0])
