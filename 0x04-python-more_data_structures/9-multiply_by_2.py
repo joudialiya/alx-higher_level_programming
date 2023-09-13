ace34244d0de3f7ca8a35fb26736f3a489da2708
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-13 12:05:21 -0700

    task 9, typo

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     cpy = a_dictionary.copy()
     for key, value in cpy.items():
-        cpy.update({key, value * 2})
+        cpy.update({key: value * 2})
     return (cpy)
