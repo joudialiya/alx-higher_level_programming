125f718fe026385eb7f7b7ebc02c978bbca3b38d
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-13 07:53:11 -0700

    task 4, returning a set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,4 +3,4 @@ def only_diff_elements(set_1, set_2):
     res = []
     res.extend(filter(lambda e: e not in set_1, set_2))
     res.extend(filter(lambda e: e not in set_2, set_1))
-    return (res)
+    return (set(res))
