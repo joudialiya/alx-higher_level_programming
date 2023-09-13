5b74d7bf74687954dee4547a368e8f1901301b9a
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-13 12:09:08 -0700

    task 10, typo

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     m = (None, None)
-    if dictionary:
+    if a_dictionary:
         for key, value in a_dictionary.items():
             if (not m[1]) or value > m[1]:
                 m = (key, value)
