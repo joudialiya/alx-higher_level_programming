2108af017652217e6127c1343c5c0b95b06d58ba
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-13 07:50:09 -0700

    task 4, typo

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,5 @@
 def only_diff_elements(set_1, set_2):
     res = []
     res.extend(filter(lambda e: e not in set_1, set_2))
-    res.entend(filter(lambda e: e not in set_2, set_1))
+    res.extend(filter(lambda e: e not in set_2, set_1))
     return (res)
