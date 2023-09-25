0bc216b54b6b38ca4d0d469303bdd131f5d58c7a
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-25 08:46:26 -0700

    pycodestyle check + i leared about how to pass a pack args to another function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_function(fct, *args):
     try:
-        return (fct(args))
+        return (fct(*args))
     except Exception as err:
         sys.stderr.write("Exception: {:s}\n".format(str(err)))
         return (None)
