a03978e25600a301ddb690550252bf12113c9a2d
Lagzal <mlagzal.sayhi@gmail.com>
2023-09-25 11:25:49 +0100

    Mandatory tasks

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    r = 0
+
+    try:
+        r = a / b
+        return (r)
+    except:
+        return (None)
+    finally:
+        print("Inside result: {}".format(r))
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    for i in range(0, list_length):
+        r = 0;
+        try:
+            r = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            res.append(r)
+    return (res)
