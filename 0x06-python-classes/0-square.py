cdd7f96993a1f6ad34e5425bdc1b7953af2148a4
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-26 04:07:33 -0700

    Add module doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-
+"""
+The purpose of this module is to grasp OOP in python
+"""
 class Square:
     """
     Square Class
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+The purpose of this module is to grasp OOP in python
+"""
 
 
 class Square:
