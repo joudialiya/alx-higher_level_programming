ecbb243e9bacc8385466bb76be574935f8bfb700
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-26 14:55:58 -0700

    10. pycodecheck

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,7 +9,7 @@ class MagicClass:
         """Constructor"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-                raise TypeError("radius must be a number")
+            raise TypeError("radius must be a number")
         else:
             self.__radius = radius
 
