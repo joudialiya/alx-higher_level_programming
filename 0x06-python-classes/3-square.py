5d5f2a5c26afae2ea56752c75925c66c7dd811fb
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-26 04:12:47 -0700

    3. pycodecheck

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,6 +17,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """
         Calculate the area of the square based on size
