f384f12748dbc2506d2a02ac933bb5571efc6cd5
joudialiya <mlagzal.sayhi@gmail.com>
2023-10-03 17:00:31 +0000

    30. using __slots__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,9 +4,4 @@
 
 class LockedClass:
     """Locked class"""
-    def __setattr__(self, key, value):
-        """custom __setasttr__"""
-        if key != "first_name":
-            msg = "'LockedClass' object has no attribute '{}'".format(key)
-            raise AttributeError(msg)
-        object.__setattr__(self, key, value)
+    __slots__ = ("first_name")
