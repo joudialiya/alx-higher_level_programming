aa7c529e6abf8b02a975119031458838f9bd6d91
joudialiya <mlagzal.sayhi@gmail.com>
2023-10-04 01:19:24 +0000

    29. every thing is object indeed

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    return (", ".join(["BestSchool"]  * (magic_string.n := magic_string.n + 1)))
-magic_string.n = 0
+def magic_string(listo=[]):
+    listo.append("BestSchool")
+    return (", ".join(listo))
