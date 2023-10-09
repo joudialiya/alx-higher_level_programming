692a1505ed7b8d128a31a7c755462a32bd3c8526
joudialiya <mlagzal.sayhi@gmail.com>
2023-10-09 11:23:51 +0000

    6.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Geometry module """
+
+
+class BaseGeometry:
+    """ Base class """
+    def area(self):
+        """calculate area"""
+        raise Exception("area() is not implemented")    
