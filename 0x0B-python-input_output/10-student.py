cf360c47abdc32c01d1c11943a1acbf4cc4e58f1
joudialiya <mlagzal.sayhi@gmail.com>
2023-10-11 01:40:49 +0000

    #10.

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Lerning about __dict__ and json"""
+
+
+class Student:
+    """Simple Student Class"""
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """To string"""
+        if not attrs:
+            return self.__dict__
+        return dict(filter(lambda e: e[0] in attrs, self.__dict__.items()))
