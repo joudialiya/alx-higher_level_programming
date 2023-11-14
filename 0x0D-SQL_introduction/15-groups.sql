2803c30becbec7c7be2834aaf2607e1b47d6929a
joudialiya <mlagzal.sayhi@gmail.com>
2023-11-14 14:40:57 +0000

    # 15 - 16

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- groupe by score
+SELECT score, COUNT(*) as 'number'
+FROM second_table
+GROUP BY score
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- select safe
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC
