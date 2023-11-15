8b359da9746c0e17504785ce7e31ea1e6e365b57
joudialiya <mlagzal.sayhi@gmail.com>
2023-11-15 17:19:56 +0000

    6. chnage the table name

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 use hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa(
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS states(
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
