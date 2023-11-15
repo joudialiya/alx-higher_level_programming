ce2eafbbe337f3ba0acb934cf5fa0c837358f401
joudialiya <mlagzal.sayhi@gmail.com>
2023-11-15 19:12:54 +0000

    8.

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,8 @@
 -- select
-SELECT id, state_id, name
+SELECT id, name
 FROM cities
 WHERE state_id = (
     SELECT id
     FROM states
-    WHERE name = "California"
-)
+    WHERE name = "California")
 ORDER BY id;
