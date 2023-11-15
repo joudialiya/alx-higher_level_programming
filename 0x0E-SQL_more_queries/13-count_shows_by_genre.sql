f095465eadebee730a526f96a8f95ca5248af7ce
joudialiya <mlagzal.sayhi@gmail.com>
2023-11-15 19:47:51 +0000

    13. fix syntax

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- no shows y genre
-SELECT tv_genres.name as 'genre', COUNT(*) as 'number_of_shows'
+SELECT tv_genres.name AS 'genre', COUNT(*) as 'number_of_shows'
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
