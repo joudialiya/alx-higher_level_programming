e22b6d8f6e8e9a5349a71ccae0ff79506ebba76f
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-11 14:27:11 +0000

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,8 @@
 
 const n = parseInt(process.argv[2]);
 
-if (isNaN(n))
-    console.log('Not a number');
-else
-    console.log(n);
+if (isNaN(n)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: '+ n);
+}
