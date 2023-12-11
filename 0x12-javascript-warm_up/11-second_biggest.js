570e8ad8dd46fbc806fe3654d02ce67f397e07b8
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-11 16:15:25 +0000

    11.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 
-let nums = [];
-
-for (let i = 2; i < process.argv.length; ++i) {
-  nums.push(parseInt(process.argv[i]));
-}
-nums.sort();
-nums = nums.filter(n => !isNaN(n));
-if (nums.length <= 2) {
-  console.log('0');
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-  console.log(`${nums[nums.length - 2]}`);
+  const list = process.argv.sort();
+  console.log(list[list.length - 2]);
 }
