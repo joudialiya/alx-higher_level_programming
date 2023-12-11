ffc6763edcc8dfe83b94ec2919f7a1ad3b8dcf70
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-11 15:06:37 +0000

    7-10

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const n = parseInt(process.argc[2]);
+
+if (!isNaN(n)) {
+  for (let i = 0; i < n; ++i) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const n = parseInt(process.argv[2]);
+
+if (!isNaN(n)) {
+  let str = '';
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < n; ++j) {
+      str += 'x';
+    }
+    str += '\n';
+  }
+  str = str.slice(0, str.length - 1);
+  console.log(str);
+} else {
+  console.log('Missing size');
+}
