cdb9f5b856b19af0cc7bc98cfcf033232ad8c69a
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-11 16:03:14 +0000

    11. refactor

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,8 +6,9 @@ for (let i = 2; i < process.argv.length; ++i) {
   nums.push(parseInt(process.argv[i]));
 }
 nums = nums.sort();
+nums.pop();
 if (nums.length < 2) {
   console.log('0');
 } else {
-  console.log(`${nums[nums.length - 2]}`);
+  console.log(`${nums.pop()}`);
 }
