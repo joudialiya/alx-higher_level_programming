0bfdfee22d76a4e0fdd3cafe1097f7ba7eff3aae
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-13 02:36:15 +0000

    102.

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,7 +3,7 @@ const fs = require('node:fs');
 try {
   fs.writeFileSync(
     process.argv[4],
-    fs.readFileSync(process.argv[2]) +
+    fs.readFileSync(process.argv[2]) + '\n'
     fs.readFileSync(process.argv[3])
   );
 } catch (e) {
