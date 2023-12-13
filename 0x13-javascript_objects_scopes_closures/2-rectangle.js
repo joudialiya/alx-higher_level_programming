c2da6eb39ce2e11f709b2aeedd950d0f9d5833e0
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-13 00:47:37 +0000

    2.

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // Rectangle class
-
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w = 0, h = 0) {
-    if (w >= 0 && h >= 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-};
+}
+
+module.exports = Rectangle;
