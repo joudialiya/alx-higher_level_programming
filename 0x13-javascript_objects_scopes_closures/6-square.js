40ec9a7a1850783700feba2f70760223dfa3a14c
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-13 01:34:34 +0000

    6.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 // Rectangle class
-const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
