3fcf5f52204805d37551e56e992dacea26da0b55
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-13 01:42:53 +0000

    8.

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Rectangle class
+
+exports.esrever = function (list) {
+  const cpy = [];
+  for (let i = 0; i < list.length; ++i) {
+    cpy.push(list.length - 1 - i);
+  }
+  return cpy;
+};
