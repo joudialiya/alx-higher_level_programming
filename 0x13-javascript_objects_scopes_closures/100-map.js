68b7cb05d96e2b2f0fba8d5898344baa3ec92fbd
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-13 02:11:01 +0000

    100.

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Rectangle class
 
-const list = require('./100-list.js');
+const list = require('./100-data').list;
 const r = list.map((e, i) => i * e);
 console.log(list);
 console.log(r);
