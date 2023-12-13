e57538a4357cd001887856756acc8ce54f6ee6c0
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-13 02:35:18 +0000

    102.

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('node:fs');
+try {
+  fs.writeFileSync(
+    process.argv[4],
+    fs.readFileSync(process.argv[2]) +
+    fs.readFileSync(process.argv[3])
+  );
+} catch (e) {
+  console.log(e);
+}
