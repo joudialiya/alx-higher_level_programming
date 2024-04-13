14030681ebb5a444c970bff929b658eb641b16a1
Lagzal <mlagzal.sayhi@gmail.com>
2024-04-13 15:45:33 +0000

    2.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""fetch states module"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    searched_name  = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{:s}' ORDER BY id ASC".format(searched_name))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
