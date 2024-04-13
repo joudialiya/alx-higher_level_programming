786a9ac093f7bfe08b830e59fa56cf98eef34dbe
Lagzal <mlagzal.sayhi@gmail.com>
2024-04-13 22:24:21 +0000

    100.

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -16,5 +16,5 @@ class State(Base):
     name = Column(String(128), nullable=False)
     cities = relationship(
         "City",
-        backref=backref("State"),
-        cascade="all, delete-orphan")
+        backref="state",
+        cascade="all, delete")
